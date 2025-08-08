4d4d6312aed18217eb56aa57ba0bd902d5c64f59
Anuradha Karuppiah <anuradhak@nvidia.com>
2025-08-08 10:53:44 -0700

    Fix the artifactory component name to aiqtoolkit for all packages (#603)
    Closes
    
    ## By Submitting this PR I confirm:
    - I am familiar with the [Contributing Guidelines](https://github.com/NVIDIA/NeMo-Agent-Toolkit/blob/develop/docs/source/resources/contributing.md).
    - We require that all contributors "sign-off" on their commits. This certifies that the contribution is your original work, or you have rights to submit it under the same license, or a compatible license.
      - Any contribution which contains commits that are not Signed-Off will not be accepted.
    - When the PR is ready for review, new or existing tests cover these changes.
    - When the PR is ready for review, the documentation is up to date with these changes.
    
    Authors:
      - Anuradha Karuppiah (https://github.com/AnuradhaKaruppiah)
    
    Approvers:
      - David Gardner (https://github.com/dagardner-nv)
    
    URL: https://github.com/NVIDIA/NeMo-Agent-Toolkit/pull/603

--- ci/scripts/gitlab/artifactory_upload.sh
+++ ci/scripts/gitlab/artifactory_upload.sh
@@ -38,6 +38,8 @@ AIQ_ARCH="any"
 AIQ_OS="any"
 
 AIQ_COMPONENTS=("nvidia-nat" "aiqtoolkit")
+# We need to fix the name of the component in artifactory to aiqtoolkit
+ARTIFACTORY_COMPONENT_FIXED_NAME="aiqtoolkit"
 
 WHEELS_BASE_DIR="${CI_PROJECT_DIR}/.tmp/wheels"
 
@@ -110,13 +112,13 @@ if [[ "${UPLOAD_TO_ARTIFACTORY}" == "true" ]]; then
                 RELATIVE_PATH="${WHEEL_FILE#${WHEELS_BASE_DIR}/}"
                 RELATIVE_PATH=$(echo "${RELATIVE_PATH}" | sed -e 's|^nvidia-nat/|aiqtoolkit/|')
                 ARTIFACTORY_PATH="${AIQ_ARTIFACTORY_NAME}/${RELATIVE_PATH}"
-
+"
                 echo "Uploading ${WHEEL_FILE} to ${ARTIFACTORY_PATH}..."
 
                 CI=true jf rt u --fail-no-op --url="${AIQ_ARTIFACTORY_URL}" \
                     --user="${URM_USER}" --password="${URM_API_KEY}" \
                     --flat=false "${WHEEL_FILE}" "${ARTIFACTORY_PATH}" \
-                    --target-props "arch=${AIQ_ARCH};os=${AIQ_OS};branch=${GIT_TAG};component_name=${AIQ_COMPONENT_NAME};version=${GIT_TAG};release_approver=${RELEASE_APPROVER};release_status=${RELEASE_STATUS}"
+                    --target-props "arch=${AIQ_ARCH};os=${AIQ_OS};branch=${GIT_TAG};component_name=${ARTIFACTORY_COMPONENT_FIXED_NAME};version=${GIT_TAG};release_approver=${RELEASE_APPROVER};release_status=${RELEASE_STATUS}"
             done
         done
     done
