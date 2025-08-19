e05e496b8371be02cd8220116060e3f7e491eae5
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2025-08-19 09:19:03 -0700

    Fix paths for compatibility packages (#672)
    ## By Submitting this PR I confirm:
    - I am familiar with the [Contributing Guidelines](https://github.com/NVIDIA/NeMo-Agent-Toolkit/blob/develop/docs/source/resources/contributing.md).
    - We require that all contributors "sign-off" on their commits. This certifies that the contribution is your original work, or you have rights to submit it under the same license, or a compatible license.
      - Any contribution which contains commits that are not Signed-Off will not be accepted.
    - When the PR is ready for review, new or existing tests cover these changes.
    - When the PR is ready for review, the documentation is up to date with these changes.
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Anuradha Karuppiah (https://github.com/AnuradhaKaruppiah)
    
    URL: https://github.com/NVIDIA/NeMo-Agent-Toolkit/pull/672

--- ci/scripts/gitlab/artifactory_upload.sh
+++ ci/scripts/gitlab/artifactory_upload.sh
@@ -37,7 +37,9 @@ fi
 NAT_ARCH="any"
 NAT_OS="any"
 
-NAT_COMPONENTS=("nvidia-nat" "aiqtoolkit")
+# nvidia-nat itself and all of the plugins are under "nvidia-nat", while the compatibility packages are under "nat"
+NAT_COMPONENTS=("nvidia-nat" "nat")
+
 # We need to fix the name of the component in artifactory to aiqtoolkit
 ARTIFACTORY_COMPONENT_FIXED_NAME="aiqtoolkit"
 
@@ -110,7 +112,7 @@ if [[ "${UPLOAD_TO_ARTIFACTORY}" == "true" ]]; then
                 # Extract relative path to preserve directory structure, but replacing the first dir with aiqtoolkit
                 # as this is an already established path in artifactory
                 RELATIVE_PATH="${WHEEL_FILE#${WHEELS_BASE_DIR}/}"
-                RELATIVE_PATH=$(echo "${RELATIVE_PATH}" | sed -e 's|^nvidia-nat/|aiqtoolkit/|')
+                RELATIVE_PATH=$(echo "${RELATIVE_PATH}" | sed -e 's|^nvidia-nat/|aiqtoolkit/|' | sed -e 's|^nat/|aiqtoolkit/|')
                 ARTIFACTORY_PATH="${AIQ_ARTIFACTORY_NAME}/${RELATIVE_PATH}"
 
                 echo "Uploading ${WHEEL_FILE} to ${ARTIFACTORY_PATH}..."
